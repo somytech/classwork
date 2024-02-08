297d02b27346d2c46a89b170c17505a4f4113015
somytech <theengineer288@gmail.com>
2024-02-08 05:16:50 -0800

    first comit

---
+++ bin/cn_classwork.dart
@@ -0,0 +1,5 @@
+import 'package:cn_classwork/cn_classwork.dart' as cn_classwork;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cn_classwork.calculate()}!');
+}
---
+++ test/cn_classwork_test.dart
@@ -0,0 +1,8 @@
+import 'package:cn_classwork/cn_classwork.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
